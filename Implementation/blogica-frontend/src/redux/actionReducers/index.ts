e62da1ecec850f9416e619f4b3d6af3afeb5a4bc
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-09-11 15:37:52 -0400

    repo restructuring

--- Implementation/blogica-frontend/src/redux/actionReducers/index.ts
+++
@@ -1,6 +0,0 @@
-import * as rootActionReducer from "./rootReducer";
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  ...rootActionReducer,
-};
--- Implementation/blogica-frontend/src/redux/thunks/reduxApiCallers.ts
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-const reduxApiCallers = {};
-
-export default reduxApiCallers;
