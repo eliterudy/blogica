6be6d36ca75579dd1929994b9c0a2191abb43bd5
Gavin D'mello <gavindmello@Gavins-MBP.hsd1.ma.comcast.net>
2022-11-02 22:34:35 -0400

    cors trial

--- Implementation/blogica-backend/config/cors.js
+++ Implementation/blogica-backend/config/cors.js
@@ -8,12 +8,15 @@ const whitelist = [
   "https://localhost:3001",
   "http://localhost:3001",
   "http://localhost:3002",
+  "https://blogica.netlify.app",
+  "http://blogica.netlify.app",
   "https://blogica.netlify.app/",
+  "http://blogica.netlify.app",
 ];
 
 var corsOptionsDelegate = (req, callback) => {
   var corsOptions;
-  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+  if (whitelist.indexOf(req.header("Origin")) !== -1 || !req.header("Origin")) {
     corsOptions = {
       origin: true,
     };
